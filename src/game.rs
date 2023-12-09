9e0b9d96d10caad411ef609c11f6d04d45da27ab
kfjustis <kfjustis@gmail.com>
2023-12-09 13:09:52 -0600

    Add snow launching mechanic, update ground color, and despawn snow

--- src/game.rs
+++ src/game.rs
@@ -7,7 +7,7 @@ use bevy_scroller::{
 };
 use bevy_tweening::{lens::*, *};
 use bevy_xpbd_2d::{math::*, prelude::*};
-
+use rand::Rng;
 use std::collections::VecDeque;
 
 pub struct GamePlugin;
@@ -22,12 +22,22 @@ impl Plugin for GamePlugin {
             WorldInspectorPlugin::new()
         ));
         app.register_type::<Speed>();
-        app.add_systems(Startup, (setup_game).chain());
-        app.add_systems(Update, (anim_player, move_player).chain());
+        app.add_systems(Startup, (setup_game, setup_snow).chain());
+        app.add_systems(Update, (
+                anim_player,
+                move_player,
+                spawn_snow,
+                move_snow,
+                collide_snow,
+                remove_snow
+            ).chain());
         app.insert_resource(ClearColor(Color::rgb(0.0, 0.0, 0.0)));
         app.insert_resource(Gravity(Vector::NEG_Y * 100.0 * 10.0));
     }
 }
+
+#[derive(Resource)]
+pub struct ScrollerImages(Vec<Handle<Image>>);
 
 #[derive(Component)]
 struct PlayerCapsule;
@@ -42,8 +52,26 @@ struct Speed(f32);
 #[derive(Component)]
 struct Wall;
 
+#[derive(Component)]
+struct SnowTile;
+
 #[derive(Resource)]
-pub struct ScrollerImages(Vec<Handle<Image>>);
+struct SnowConfig {
+    // How often the snow should spawn.
+    timer: Timer,
+}
+
+// Define the collision layers
+#[derive(PhysicsLayer)]
+enum Layer {
+    Player,
+    Wall,
+    Ground,
+    Snow
+}
+
+#[derive(Component)]
+struct ToDelete;
 
 fn setup_game(
     mut commands: Commands,
@@ -115,8 +143,9 @@ fn setup_game(
         },
         RigidBody::Dynamic,
         Collider::capsule(32.0, 16.0),
-        LockedAxes::new()
-            .lock_rotation(),
+        CollisionLayers::new([Layer::Player],
+            [Layer::Ground, Layer::Wall, Layer::Snow]),
+        LockedAxes::new().lock_rotation(),
         Friction::ZERO.with_combine_rule(CoefficientCombine::Min),
         Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
         Mass(1.0),
@@ -146,17 +175,20 @@ fn setup_game(
 
     // Spawn the ground.
     commands.spawn((
+        Name::new("Ground"),
         SpriteBundle {
             sprite: Sprite {
-                color: Color::rgb(0.63, 0.46, 0.06),
-                custom_size: Some(Vec2::new(320.0, 32.0)),
+                color: Color::rgb(0.95, 0.95, 1.0),
+                custom_size: Some(Vec2::new(640.0, 32.0)),
                 ..default()
             },
             transform: Transform::from_xyz(0.0, -104.0, 200.0),
             ..default()
         },
         RigidBody::Static,
-        Collider::cuboid(320.0, 32.0)
+        Collider::cuboid(640.0, 32.0),
+        CollisionLayers::new([Layer::Ground],
+            [Layer::Player, Layer::Snow]),
     ));
 
     // Spawn the walls.
@@ -170,6 +202,8 @@ fn setup_game(
         },
         RigidBody::Static,
         Collider::cuboid(64.0, 64.0),
+        CollisionLayers::new([Layer::Wall],
+            [Layer::Player]),
     ));
     commands.spawn((
         Name::new("WallEntityR"),
@@ -181,6 +215,8 @@ fn setup_game(
         },
         RigidBody::Static,
         Collider::cuboid(64.0, 64.0),
+        CollisionLayers::new([Layer::Wall],
+            [Layer::Player]),
     ));
 }
 
@@ -248,5 +284,96 @@ fn move_player(
 
         // Apply friction.
         linear_vel.x *= 0.8;
+    }
+}
+
+fn setup_snow(
+    mut commands: Commands
+) {
+    commands.insert_resource(
+        SnowConfig {
+            // Create the repeating timer.
+            timer: Timer::new(std::time::Duration::from_millis(500), TimerMode::Repeating)
+        }
+    );
+}
+
+fn spawn_snow(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    time: Res<Time>,
+    mut config: ResMut<SnowConfig>
+) {
+    // Tick the snow timer.
+    config.timer.tick(time.delta());
+
+    if config.timer.finished() {
+        // Pick a random snow sprite each time.
+        let mut rng = rand::thread_rng();
+        let sprites = vec!["snow_1.png", "snow_2.png"];
+        let sprite_idx: usize = rng.gen_range(0..sprites.len());
+        let path = sprites[sprite_idx];
+        // Spawn the snow sprite with its physics components.
+        commands.spawn((
+            Name::new("SnowTile"),
+            SnowTile,
+            SpriteBundle {
+                texture: asset_server.load(path),
+                // Just above the player in z-order.
+                transform: Transform::from_xyz(192.0, 0.0, 300.0),
+                ..default()
+            },
+            RigidBody::Dynamic,
+            Collider::cuboid(32.0, 32.0),
+            CollisionLayers::new([Layer::Snow],
+                [Layer::Player, Layer::Ground]),
+            //LockedAxes::new().lock_rotation(),
+            Friction::ZERO.with_combine_rule(CoefficientCombine::Min),
+            Restitution::ZERO.with_combine_rule(CoefficientCombine::Min),
+            Mass(1.0),
+            Speed(24.0)
+        ));
+    }
+}
+
+fn move_snow(
+    mut snow: Query<&mut LinearVelocity, With<SnowTile>>
+) {
+    let snow_speed = 1.0;
+    for mut linear_vel in &mut snow {
+        linear_vel.x += Vec2::new(-1.0, 0.0).normalize_or_zero().x * snow_speed;
+    }
+}
+
+fn collide_snow(
+    mut commands: Commands,
+    keys: Res<Input<KeyCode>>,
+    player: Query<Entity, With<PlayerCapsule>>,
+    mut collisions: Query<(Entity, &mut LinearVelocity, &CollidingEntities), With<SnowTile>>
+) {
+    let force = 160.0;
+    let hold_action = keys.any_pressed([KeyCode::Space]);
+    for (entity, mut linear_vel, colliding_entities) in &mut collisions {
+        if hold_action && colliding_entities.contains(&player.single())
+        {
+            linear_vel.x += Vec2::new(1.0, 0.0).x * force;
+            linear_vel.y += Vec2::new(0.0, 2.0).y * force;
+
+            // Add the toDelete component.
+            commands.entity(entity).insert(ToDelete);
+        }
+    }
+}
+
+fn remove_snow(
+    mut commands: Commands,
+    snow: Query<(Entity, &Transform), (With<SnowTile>, With<ToDelete>)>
+) {
+    for (entity, transform) in &snow {
+        if transform.translation.x < -130.0 || transform.translation.x > 250.0 ||
+            transform.translation.y < -100.0 || transform.translation.y > 150.0
+        {
+            commands.entity(entity).despawn_recursive();
+        }
     }
 }
