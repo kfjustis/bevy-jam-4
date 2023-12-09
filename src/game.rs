c566f634790fa4a4b30cbce965b343e7eb7c9283
kfjustis <kfjustis@gmail.com>
2023-12-09 13:51:20 -0600

    Add snow bg fx

--- src/game.rs
+++ src/game.rs
@@ -24,6 +24,7 @@ impl Plugin for GamePlugin {
         app.register_type::<Speed>();
         app.add_systems(Startup, (setup_game, setup_snow).chain());
         app.add_systems(Update, (
+                anim_snow_fx,
                 anim_player,
                 move_player,
                 spawn_snow,
@@ -73,9 +74,19 @@ enum Layer {
 #[derive(Component)]
 struct ToDelete;
 
+#[derive(Component)]
+struct AnimationIndices {
+    first: usize,
+    last: usize,
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
 fn setup_game(
     mut commands: Commands,
-    asset_server: Res<AssetServer>
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>
 ) {
     commands.spawn(Camera2dBundle {
         projection: OrthographicProjection {
@@ -129,6 +140,22 @@ fn setup_game(
             },
         ));
     });
+
+    // Spawn the snow at ~80 z-index.
+    let texture_handle = asset_server.load("snow_fx_sheet_4.png");
+    let texture_atlas = TextureAtlas::from_grid(texture_handle, Vec2::new(320.0, 240.0), 4, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+    let animation_indices = AnimationIndices{first: 0, last: 3};
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(animation_indices.first),
+            transform: Transform::from_xyz(0.0, 0.0, 80.0),
+            ..default()
+        },
+        animation_indices,
+        AnimationTimer(Timer::from_seconds(0.25, TimerMode::Repeating))
+    ));
 
     // Spawn the player with its physics, sprite, and tween animations.
     // The sprite is a child of the capsule/SpatialBundle so it can
@@ -218,6 +245,22 @@ fn setup_game(
         CollisionLayers::new([Layer::Wall],
             [Layer::Player]),
     ));
+}
+
+fn anim_snow_fx (
+    time: Res<Time>,
+    mut query: Query<(&AnimationIndices, &mut AnimationTimer, &mut TextureAtlasSprite)>
+) {
+    for (indices, mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = if sprite.index == indices.last {
+                indices.first
+            } else {
+                sprite.index + 1
+            };
+        }
+    }
 }
 
 fn anim_player(
